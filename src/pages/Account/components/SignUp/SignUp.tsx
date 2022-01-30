14227afd1551ca5826d7f9566fb2558983cb6912
g0m3s <gom3s.g@gmail.com>
2022-01-30 17:51:18 -0300

    feat: signUp ui

--- src/pages/Account/components/SignUp/SignUp.tsx
+++ src/pages/Account/components/SignUp/SignUp.tsx
@@ -4,14 +4,15 @@ import * as yup from 'yup'
 import { useSnackbar } from 'notistack'
 import { useForm } from 'react-hook-form'
 import { useHistory } from 'react-router'
-import { Box, Button, Stack } from '@mui/material'
 import { app } from '../../../../FIREBASECONFIG.js'
 import { User, Account } from '../../../../types/user'
 import { yupResolver } from '@hookform/resolvers/yup'
+import { Box, Button, Grid, Stack } from '@mui/material'
 import { getDatabase, ref, set } from 'firebase/database'
+import { useIsMobile } from '../../../../hooks/useIsMobile'
 import { useUserStore } from '../../../../store/user/reducer'
-import { Container, HfField, TextInput } from '../../../../components'
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
+import { Container, HfField, TextInput, PasswordInput } from '../../../../components'
 
 type formValuesType = Omit<Account, 'isAdmin' | 'id'> & { senha: string | null }
 
@@ -19,6 +20,7 @@ export const SignUp: React.FC = () => {
   const auth = getAuth()
   const db = getDatabase(app)
   const history = useHistory()
+  const isMobile = useIsMobile()
   const { enqueueSnackbar } = useSnackbar()
   const { operations: { updateUser } } = useUserStore()
 
@@ -86,86 +88,120 @@ export const SignUp: React.FC = () => {
   })
 
   return (
-    <Box sx={{ backgroundColor: '#3b3b3b' }}>
-      <Container>
+    <Container>
+      <Box sx={{ padding: 3 }}>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <Stack>
-            <HfField
-              name='email'
-              inputType='flat'
-              control={control}
-              label='Seu e-mail'
-              component={TextInput}
-              errorMessage={errors.email?.message}
-            />
-            <HfField
-              name='senha'
-              inputType='flat'
-              control={control}
-              label='Senha'
-              component={TextInput}
-              errorMessage={errors.senha?.message}
-            />
-            <HfField
-              name='name'
-              inputType='flat'
-              control={control}
-              label='Seu nome'
-              component={TextInput}
-              errorMessage={errors.name?.message}
-            />
-            <HfField
-              name='phone'
-              inputType='flat'
-              control={control}
-              label='Telefone'
-              component={TextInput}
-              errorMessage={errors.phone?.message}
-            />
-            <HfField
-              name='city'
-              inputType='flat'
-              control={control}
-              label='Cidade'
-              component={TextInput}
-              errorMessage={errors.city?.message}
-            />
-            <HfField
-              name='cep'
-              inputType='flat'
-              control={control}
-              label='CEP'
-              component={TextInput}
-              errorMessage={errors.cep?.message}
-            />
-            <HfField
-              name='address'
-              inputType='flat'
-              control={control}
-              label='Rua'
-              component={TextInput}
-              errorMessage={errors.address?.message}
-            />
-            <HfField
-              name='district'
-              inputType='flat'
-              control={control}
-              label='Bairro'
-              component={TextInput}
-              errorMessage={errors.district?.message}
-            />
-            <HfField
-              name='houseNumber'
-              inputType='flat'
-              control={control}
-              label='Número da casa'
-              component={TextInput}
-              errorMessage={errors.houseNumber?.message}
-            />
-            <Button type='submit'>Entrar</Button>
+          <Grid spacing={2} container>
+            <Grid item md={isMobile ? 12 : 4} lg={isMobile ? 12 : 4} xs={isMobile ? 12 : 4}>
+              <HfField
+                name='email'
+                inputType='flat'
+                control={control}
+                label='Seu e-mail'
+                component={TextInput}
+                errorMessage={errors.email?.message}
+              />
+            </Grid>
+            <Grid item md={isMobile ? 12 : 4} lg={isMobile ? 12 : 4} xs={isMobile ? 12 : 4}>
+              <HfField
+                name='senha'
+                label='Senha'
+                inputType='flat'
+                control={control}
+                component={PasswordInput}
+                errorMessage={errors.senha?.message}
+              />
+            </Grid>
+            <Grid item md={isMobile ? 12 : 4} lg={isMobile ? 12 : 4} xs={isMobile ? 12 : 4}>
+              <HfField
+                name='name'
+                inputType='flat'
+                control={control}
+                label='Seu nome'
+                component={TextInput}
+                errorMessage={errors.name?.message}
+              />
+            </Grid>
+            <Grid item md={isMobile ? 12 : 4} lg={isMobile ? 12 : 4} xs={isMobile ? 12 : 4}>
+              <HfField
+                name='phone'
+                inputType='flat'
+                control={control}
+                label='Telefone'
+                component={TextInput}
+                errorMessage={errors.phone?.message}
+              />
+            </Grid>
+            <Grid item md={isMobile ? 12 : 4} lg={isMobile ? 12 : 4} xs={isMobile ? 12 : 4}>
+              <HfField
+                name='city'
+                inputType='flat'
+                control={control}
+                label='Cidade'
+                component={TextInput}
+                errorMessage={errors.city?.message}
+              />
+            </Grid>
+            <Grid item md={isMobile ? 12 : 4} lg={isMobile ? 12 : 4} xs={isMobile ? 12 : 4}>
+              <HfField
+                name='cep'
+                inputType='flat'
+                control={control}
+                label='CEP'
+                component={TextInput}
+                errorMessage={errors.cep?.message}
+              />
+            </Grid>
+            <Grid item md={isMobile ? 12 : 4} lg={isMobile ? 12 : 4} xs={isMobile ? 12 : 4}>
+              <HfField
+                name='address'
+                inputType='flat'
+                control={control}
+                label='Rua'
+                component={TextInput}
+                errorMessage={errors.address?.message}
+              />
+            </Grid>
+            <Grid item md={isMobile ? 12 : 4} lg={isMobile ? 12 : 4} xs={isMobile ? 12 : 4}>
+              <HfField
+                name='district'
+                inputType='flat'
+                control={control}
+                label='Bairro'
+                component={TextInput}
+                errorMessage={errors.district?.message}
+              />
+            </Grid>
+            <Grid item md={isMobile ? 12 : 4} lg={isMobile ? 12 : 4} xs={isMobile ? 12 : 4}>
+              <HfField
+                name='houseNumber'
+                inputType='flat'
+                control={control}
+                label='Número da casa'
+                component={TextInput}
+                errorMessage={errors.houseNumber?.message}
+              />
+            </Grid>
+          </Grid>
+          <Stack mt={3} alignItems='center' justifyContent='center' sx={{ width: '100%' }}>
+            <Box
+              type='submit'
+              component={Button}
+              sx={{
+                padding: 2,
+                color: 'white',
+                borderRadius: 3,
+                alignItems: 'center',
+                cursor: 'pointer',
+                bgcolor: '#a9cf46',
+                width: isMobile ? '80vw' : 'auto',
+                '&:hover': { backgroundColor: '#a9cf46' },
+                boxShadow: '0px 0px 5px rgba(169, 207, 70, 1)',
+              }}
+            >Criar conta</Box>
           </Stack>
         </form>
-      </Container>
-    </Box>
+      </Box>
+    </Container>
   )
 }
